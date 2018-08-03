c7229a52ce7996c0e3d5efbe105543a05b6d1da7
Oscarlin Matos <oscarlino.matos@gmail.com>
2018-08-03 03:07:34 -0400

    Creando la estructura html

---
+++ assets/js/lib/menu.js
@@ -0,0 +1,87 @@
+
+var iconPath = "assets/img/icons/omg.png"
+var menu = [
+        {
+            header: {
+                title: "Inicio",
+                icon: iconPath,
+                style: ""
+            },
+            options: []
+        },
+        {
+            header: {
+                title: "Vehiculos",
+                icon: iconPath,
+                style: ""
+            },
+            options: [
+                {
+                    title: "Vehiculos en oferta",
+                    icon: iconPath,
+                    style: ""
+                },
+                {
+                    title: "Vehiculos usados",
+                    icon: iconPath + "vehiculousado.ico",
+                    style: ""
+                },
+                {
+                    title: "Vehiculos nuevos",
+                    icon: iconPath,
+                    style: ""
+                }
+            ]
+        },
+        {
+            header: {
+                title: "Directorio",
+                icon: iconPath,
+                style: ""
+            },
+            options: [
+                {
+                    title: "Dealers",
+                    icon: iconPath,
+                    style: ""
+                },
+                {
+                    title: "Financiamientos",
+                    icon: iconPath,
+                    style: ""
+                },
+
+                {
+                    title: "Seguros",
+                    icon: iconPath,
+                    style: ""
+                },
+                {
+                    title: "Rent-car",
+                    icon: iconPath,
+                    style: ""
+                },
+                {
+                    title: "Autoadornos",
+                    icon: iconPath,
+                    style: ""
+                },
+                {
+                    title: "Repuestos",
+                    icon: iconPath,
+                    style: ""
+                },
+                {
+                    title: "Talleres",
+                    icon: iconPath,
+                    style: ""
+                },
+                {
+                    title: "Consesionarios",
+                    icon: iconPath,
+                    style: ""
+                }
+            ]
+        }
+    ]
+
